57a7829d02873830b1505229e353fd97dabd84e4
PJ Frias <peejfrias@gmail.com>
2018-01-23 22:02:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,8 +74,9 @@ class Student
     self.new_from_db(row)
   end
 
-  def update(name)
-    # binding.pry
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
